3f737cb96768deb53b0974802318beb1f639bd13
Steffen <s.bueschking@campus.tu-berlin.de>
2018-12-20 22:31:56 +0100

    i2c read and write master ready

--- Driver/I2C/master/reg-access.c
+++ Driver/I2C/master/reg-access.c
@@ -1,50 +1,48 @@
 #include "register.h"
 
-#define MSMS (1 << 2)
 #define TX (1 << 3)
-#define TXAK (1 << 4)
+#define RX_FIFO_EMPTY (1 << 6)
+
+#define START_BIT (1 << 8)
+#define STOP_BIT (1 << 9)
+
+#define WRITE_BIT 0
+#define READ_BIT 1
 
 unsigned char readI2CRegisterMaster(unsigned int base_addr, unsigned char addr) {
-    /* Write I2C Address on TX_FIFO */
-    *(unsigned int*)(base_addr + TX_FIFO) = addr;
-    
-    /* Generate START signal */
-    *(unsigned int*)(base_addr + CR) |= MSMS;
+    /* Switch to Transmit Mode */
+    *(unsigned int*)(base_addr + CR) |= TX;
+
+    /* Set start bit, I2C address and read bit */
+    unsigned int tx_addr_entry = START_BIT | (addr << 1) | READ_BIT;
+    *(unsigned int*)(base_addr + TX_FIFO) = tx_addr_entry;
+
+    /* Set stop bit and data byte count to 1 */
+    unsigned int tx_data_entry = STOP_BIT | 1;
+    *(unsigned int*)(base_addr + TX_FIFO) = tx_data_entry;
 
     /* Switch to Receive Mode */
-    *(unsigned int*)(base_addr + CR) &= (~TX);
+    *(unsigned int*)(base_addr + CR) &= ~TX;
 
-    /* Wait for RX_FIFO empty interrupt */
-    //TODO
-
-    /* Acknoledge received data */
-    *(unsigned int*)(base_addr + CR) |= TXAK;
+    /* Wait for RX_FIFO to be filled */
+    while(*(unsigned int*)(base_addr + SR) != RX_FIFO_EMPTY);
 
     /* Read data from RX_FIFO */
     unsigned char data = *(unsigned int*)(base_addr + RX_FIFO);
 
-    /* Generate STOP signal */
-    *(unsigned int*)(base_addr + CR) &= (~MSMS);
+    return data;
 
 }
 
 void writeI2CRegisterMaster(unsigned int base_addr, unsigned char addr, unsigned char data) {
-    /* Write I2C Address on TX_FIFO */
-    *(unsigned int*)(base_addr + TX_FIFO) = addr;
-
-    /* Write data to TX_FIFO */
-    *(unsigned int*)(base_addr + TX_FIFO) = data;
-
-    /* Generate START signal */
-    *(unsigned int*)(base_addr + CR) |= MSMS;
-
     /* Switch to Transmit Mode */
     *(unsigned int*)(base_addr + CR) |= TX;
 
-    /* Wait for TX_FIFO empty interrupt */
+    /* Set start bit, I2C address and write bit */
+    unsigned int tx_addr_entry = START_BIT | (addr << 1) | WRITE_BIT;
+    *(unsigned int*)(base_addr + TX_FIFO) = tx_addr_entry;
 
-    /* Generate STOP signal */
-    *(unsigned int*)(base_addr + CR) &= (~MSMS);
-
-
+    /* Set stop bit and data byte*/
+    unsigned int tx_data_entry = STOP_BIT | data;
+    *(unsigned int*)(base_addr + TX_FIFO) = tx_data_entry;
 }
