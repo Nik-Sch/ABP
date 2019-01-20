7424230c834b4d9623f2a45a8739bcafc51f5324
Nik-Sch <nik.schelten@gmail.com>
2019-01-20 15:00:12 +0100

    small python change

--- Vivado/FFT/python.test/test_generator.py
+++ Vivado/FFT/python.test/test_generator.py
@@ -25,11 +25,11 @@ def sliding_dft(data, N, Fs):
             for f in range(int(N/2)):
                 X[f] = ((X[f] + data[i]) * np.complex(np.cos(2*np.pi*f/N), -np.sin(2*np.pi*f/N)))
 
-        if i % (N / 2) == 0:
+        if i % N == 0:
             fig = plt.figure()
-            # plt.ylim((0, 500000))
-            plt.plot(np.abs(X))
-            # plt.plot(freq[0:int(len(freq)/2)], np.abs(X))
+            plt.ylim((0, 500000))
+            # plt.plot(np.abs(X))
+            plt.plot(freq[0:int(len(freq)/2)], np.abs(X))
             plt.title(f't={i:d}')
             plt.xlabel('Frequency [Hz]')
             plt.ylabel('freq amplitude (no real unit)')
@@ -43,17 +43,17 @@ def sliding_dft(data, N, Fs):
 
 
 # created testdata
-f1 = 550
-f2 = 1100
-Fs = 8000
-time = 1
-t = np.linspace(0, time, Fs*time)
-y1 = (1 << 14) * np.sin(2*np.pi*f1 * t[0:np.int64(np.floor(len(t)/2))])
-y2 = (1 << 14) * np.sin(2*np.pi*f2 * t[np.int64(np.ceil(len(t)/2)):len(t)])
-y = np.concatenate((y1, y2))
+# f1 = 550
+# f2 = 1100
+# Fs = 8000
+# time = 1
+# t = np.linspace(0, time, Fs*time)
+# y1 = (1 << 14) * np.sin(2*np.pi*f1 * t[0:np.int64(np.floor(len(t)/2))])
+# y2 = (1 << 14) * np.sin(2*np.pi*f2 * t[np.int64(np.ceil(len(t)/2)):len(t)])
+# y = np.concatenate((y1, y2))
 
 # sample guitar 22.05kHz
-# Fs, y = wavfile.read('./sample_guitar.wav')
+Fs, y = wavfile.read('./sample_guitar.wav')
 
 print(Fs, len(y))
 sliding_dft(y, 512, Fs);
