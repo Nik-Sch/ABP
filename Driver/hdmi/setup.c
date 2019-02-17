875a096ef84d35ba73f908dade98575dfae77556
Steffen Büschking <universeleid.sunny@live.de>
2019-02-17 17:34:50 +0100

    declaration added, driver function minor changes

--- Driver/hdmi/setup.c
+++ Driver/hdmi/setup.c
@@ -1,116 +1,103 @@
 #include "setup.h"
 
-#define POWER_DOWN 0
+#define POWER_UP 0
 
-void adv7511_setup_power_up(unsigned int *mm_addr) {
-    modifyI2CRegisterMaster(&mm_addr, 0x41, 6, 6, POWER_DOWN);
+void adv7511_setup_power_up(unsigned int mm_addr) {
+    modifyI2CRegisterMaster(mm_addr, 0x41, 6, 6, POWER_UP);
 }
 
-void adv7511_setup_fixed_register(unsigned int *mm_addr) {
-    writeI2CRegisterMaster(&mm_addr, 0x96, 0x03);
-    modifyI2CRegisterMaster(&mm_addr, 0x9A, 7, 5, 0b111);
-    writeI2CRegisterMaster(&mm_addr, 0x9C, 0x30);
-    modifyI2CRegisterMaster(&mm_addr, 0x9D, 1, 0, 0b01);
-    writeI2CRegisterMaster(&mm_addr, 0xA2, 0xA4);
-    writeI2CRegisterMaster(&mm_addr, 0xA3, 0xA4);
-    writeI2CRegisterMaster(&mm_addr, 0xE0, 0xD0);
-    writeI2CRegisterMaster(&mm_addr, 0xF9, 0x00);
+void adv7511_setup_fixed_register(unsigned int mm_addr) {
+    writeI2CRegisterMaster(mm_addr, 0x96, 0x03);
+    modifyI2CRegisterMaster(mm_addr, 0x9A, 7, 5, 0b111);
+    writeI2CRegisterMaster(mm_addr, 0x9C, 0x30);
+    modifyI2CRegisterMaster(mm_addr, 0x9D, 1, 0, 0b01);
+    writeI2CRegisterMaster(mm_addr, 0xA2, 0xA4);
+    writeI2CRegisterMaster(mm_addr, 0xA3, 0xA4);
+    writeI2CRegisterMaster(mm_addr, 0xE0, 0xD0);
+    writeI2CRegisterMaster(mm_addr, 0xF9, 0x00);
 
 }
 
-void adv7511_setup_video_input(unsigned int *mm_addr) {
-    unsigned char entry;
-
-    /* Set YCbCr 4:2:2 embedded syncs */
-    entry = readI2CRegisterMaster(&mm_addr, 0x15);
-    entry |= 2;
-    writeI2CRegisterMaster(&mm_addr, 0x15, entry);
+void adv7511_setup_video_input(unsigned int mm_addr) {
+    /* Set YCbCr embedded syncs 16 Bit Color Depth */
+	modifyI2CRegisterMaster(mm_addr, 0x15, 3, 0, 0b10);
 
     /* Color depth of 12 bit */
-    entry = readI2CRegisterMaster(&mm_addr, 0x16);
-    entry |= (0b10 << 4);
-    writeI2CRegisterMaster(&mm_addr, 0x16, entry);
+	modifyI2CRegisterMaster(mm_addr, 0x16, 5, 4, 0b11);
 
-    /* Set Video Input Style 1 */
-    entry = readI2CRegisterMaster(&mm_addr, 0x16);
-    entry |= (2 << 2);
-    writeI2CRegisterMaster(&mm_addr, 0x16, entry);
+    /* Set Video Input Style 2 */
+	modifyI2CRegisterMaster(mm_addr, 0x16, 3, 2, 0b01);
 
     /* Set aspect ratio to 16x9 */
-    entry = readI2CRegisterMaster(&mm_addr, 0x17);
-    entry |= 0b1;
-    writeI2CRegisterMaster(&mm_addr, 0x17, entry);
+	modifyI2CRegisterMaster(mm_addr, 0x17, 1, 1, 0b1);
 
 }
 
-void adv7511_setup_video_output(unsigned int *mm_addr) {
-    unsigned char entry;
-
+void adv7511_setup_video_output(unsigned int mm_addr) {
     /* Set Output format to 4:4:4 */
-    entry = readI2CRegisterMaster(&mm_addr, 0x16);
-    entry &= (0b0 << 7);
-    writeI2CRegisterMaster(&mm_addr, 0x16, entry);
+	modifyI2CRegisterMaster(mm_addr, 0x16, 7, 6, 0b0);
 
     /* Set YCbCr to RGB - CSC Enable */
-    entry = readI2CRegisterMaster(&mm_addr, 0x18);
-    entry |= (0b1 << 7);
-    writeI2CRegisterMaster(&mm_addr, 0x18, entry);
+	modifyI2CRegisterMaster(mm_addr, 0x18, 7, 7, 0b1);
 
     /* Set YCbCr to RGB - CSC Scaling Factor */
-    entry = readI2CRegisterMaster(&mm_addr, 0x18);
-    entry |= (0b00 << 5);
-    writeI2CRegisterMaster(&mm_addr, 0x18, entry);
+	modifyI2CRegisterMaster(mm_addr, 0x18, 6, 5, 0b00);
 
     /* Select HDMI Mode */
-    entry = readI2CRegisterMaster(&mm_addr, 0xAF);
-    entry |= 0b1;
-    writeI2CRegisterMaster(&mm_addr, 0xAF, entry);
+	modifyI2CRegisterMaster(mm_addr, 0xAF, 1, 1, 0b1);
 
     /* Enable GC */
-    entry = readI2CRegisterMaster(&mm_addr, 0x40);
-    entry |= 0b1;
-    writeI2CRegisterMaster(&mm_addr, 0x40, entry);
+	modifyI2CRegisterMaster(mm_addr, 0x40, 7, 7, 0b1);
 
     /* Output color depth of 36 Bit/Pixel */
-    entry = readI2CRegisterMaster(&mm_addr, 0x4C);
-    entry |= 0b110;
-    writeI2CRegisterMaster(&mm_addr, 0x4C, entry);
+	modifyI2CRegisterMaster(mm_addr, 0x4C, 3, 0, 0b0110);
 
 
 }
 
-void adv7511_setup_hdcp(unsigned int *mm_addr) {
+void adv7511_setup_hdcp(unsigned int mm_addr) {
     unsigned char entry;
 
     /* Enable HDCP */
-    entry = readI2CRegisterMaster(&mm_addr, 0xAF);
-    entry |= (1 << 7);
-    writeI2CRegisterMaster(&mm_addr, 0xAF, entry);
+	modifyI2CRegisterMaster(mm_addr, 0xAF, 7, 7, 0b1);
 
     /* Wait for BKSV Interrupt */
     do {
-        entry = readI2CRegisterMaster(&mm_addr, 0x97);
+        entry = readI2CRegisterMaster(mm_addr, 0x97);
 
-    } while((entry &= (1 << 7)) == 0);
+    } while((entry & (1 << 6)) == 0);
 
     /* Write BKSV Interrupt status bit */
-    entry |= (1 << 7);
-    writeI2CRegisterMaster(&mm_addr, 0x97, entry);
+	modifyI2CRegisterMaster(mm_addr, 0x97, 6, 6, 0b1);
 }
 
-void adv7511_audio_disable(unsigned int *mm_addr) {
+void adv7511_audio_disable(unsigned int mm_addr) {
     /* Disable Audio Sample Packet */
-    unsigned char entry = readI2CRegisterMaster(&mm_addr, 0x44);
-    entry &= ~(1 << 5);
-    writeI2CRegisterMaster(&mm_addr, 0x44, entry); 
+	modifyI2CRegisterMaster(mm_addr, 0x44, 5, 5, 0b0);
 }
-void adv7511_setup(unsigned int *mm_addr) {
 
-    adv7511_setup_power_down(mm_addr);
-    adv7511_setup_fixed_register(mm_addr);
-    adv7511_setup_video_input(mm_addr);
-    adv7511_setup_video_output(mm_addr);
-    adv7511_setup_hdcp(mm_addr);
-    adv7511_audio_disable(mm_addr);
+void adv7511_setup_embedded_syncs(unsigned int mm_addr) {
+	/* Setup DE Generation for 1080p 60Hz */
+	writeI2CRegisterMaster(mm_addr, 0x35, 0x2F);
+	writeI2CRegisterMaster(mm_addr, 0x36, 0xE9);
+	writeI2CRegisterMaster(mm_addr, 0x37, 0x0F);
+	writeI2CRegisterMaster(mm_addr, 0x38, 0x00);
+	writeI2CRegisterMaster(mm_addr, 0x39, 0x43);
+	writeI2CRegisterMaster(mm_addr, 0x3A, 0x80);
 
+	/* Setup Sync Adjustment for 1080p 60Hz */
+	writeI2CRegisterMaster(mm_addr, 0xD7, 0x16);
+	writeI2CRegisterMaster(mm_addr, 0xD8, 0x02);
+	writeI2CRegisterMaster(mm_addr, 0xD9, 0xC0);
+	writeI2CRegisterMaster(mm_addr, 0xDA, 0x10);
+	writeI2CRegisterMaster(mm_addr, 0xDB, 0x05);
+	modifyI2CRegisterMaster(mm_addr, 0x17, 6, 5, 0x0);
+
+	/* Setup Embedded Sync Processing for 1080p 60Hz */
+	writeI2CRegisterMaster(mm_addr, 0x30, 0x16);
+	writeI2CRegisterMaster(mm_addr, 0x31, 0x02);
+	writeI2CRegisterMaster(mm_addr, 0x32, 0xC0);
+	writeI2CRegisterMaster(mm_addr, 0x33, 0x10);
+	writeI2CRegisterMaster(mm_addr, 0x34, 0x05);
+	modifyI2CRegisterMaster(mm_addr, 0x17, 6, 5, 0x0);
 }
--- Driver/hdmi/setup.h
+++ Driver/hdmi/setup.h
@@ -3,6 +3,23 @@
 
 #include "../i2c_master/i2c_reg.h"
 
-void adv7511_setup(unsigned int *mm_addr);
+/* Power up every single component of the chip */
+void adv7511_setup_power_up(unsigned int mm_addr);
+
+/* Setup fixed register required for configuration of the chip */
+void adv7511_setup_fixed_register(unsigned int mm_addr);
+
+/* Setup 4:2:2 YCrCb 8 Bit Color Depth as video input format */
+void adv7511_setup_video_input(unsigned int mm_addr);
+
+/* Setup 4:4:4 RGB 8 Bit Color Depth as video output format */
+void adv7511_setup_video_output(unsigned int mm_addr);
+void adv7511_setup_hdcp(unsigned int mm_addr);
+
+/* Disable audio packets in order to prevent any noises*/
+void adv7511_audio_disable(unsigned int mm_addr);
+
+/* Setup embedded syncs (VSYNC, HSYNC and DE) Generation */
+void adv7511_setup_embedded_syncs(unsigned int mm_addr);
 
 #endif
